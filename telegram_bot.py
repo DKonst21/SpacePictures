e92994cc9a2f5db8b81d308b139b112d9181d929
DKonst21 <104372191+DKonst21@users.noreply.github.com>
2023-02-19 03:23:14 +0400

    added an infinite loop to the function send_files

--- telegram_bot.py
+++ telegram_bot.py
@@ -11,18 +11,21 @@ time_delay = 4 * 60 * 60
 
 def take_files():
     catalog = "images"
+    picture_title = create_parser()
 
-    if create_parser():
-        filepath = os.path.join(catalog, create_parser())
+    if picture_title:
+        filepath = os.path.join(catalog, picture_title)
     else:
         filepath = os.path.join(catalog, random.choice(os.listdir(catalog)))
     return filepath
 
 
 def send_files(bot, telegram_chat_id):
-    with open(take_files(), 'rb') as filepath:
-        bot.send_photo(chat_id=telegram_chat_id, photo=filepath)
-        time.sleep(5)
+    while True:
+        with open(take_files(), 'rb') as filepath:
+            print(take_files())
+            bot.send_photo(chat_id=telegram_chat_id, photo=filepath)
+            time.sleep(time_delay)
 
 
 def create_parser():
