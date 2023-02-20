21c6b31448f82df18621133e074e718dfeaec9d6
DKonst21 <104372191+DKonst21@users.noreply.github.com>
2023-02-20 20:27:50 +0400

    removed the comparison in the function create_directory. Added parametr payload in download_images

--- download_images.py
+++ download_images.py
@@ -3,13 +3,12 @@ import os
 
 
 def create_directory(directory):
-    if not os.path.isdir(directory):
-        os.makedirs(directory, exist_ok=False)
+    os.makedirs(directory, exist_ok=True)
 
 
-def download_images(url, file_path):
+def download_images(url, file_path, payload=''):
 
-    response = requests.get(url)
+    response = requests.get(url, params=payload)
     response.raise_for_status()
 
     with open(file_path, 'wb') as file:
